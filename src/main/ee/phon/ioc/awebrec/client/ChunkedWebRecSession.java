b641abf5249c5cdd543f03e0a8a88a4f58534e04
tanel <tanel@viterbi.kybi>
2011-06-27 10:10:12 +0300

    Should work on Android, made compatible with Google

--- src/main/ee/phon/ioc/awebrec/client/ChunkedWebRecSession.java
+++ src/main/ee/phon/ioc/awebrec/client/ChunkedWebRecSession.java
@@ -3,6 +3,7 @@ package ee.phon.ioc.awebrec.client;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Properties;
@@ -18,7 +19,8 @@ public class ChunkedWebRecSession implements RecSession {
 	private Properties configuration = new Properties();
 
 	private HttpURLConnection connection;
-	
+	private OutputStream out;
+
 	private String result = ""; 
 	private boolean finished = false;
 
@@ -31,20 +33,21 @@ public class ChunkedWebRecSession implements RecSession {
 		// set default base URL
 		configuration.setProperty(CONF_BASE_URL, url.toExternalForm());
 	}
-	
-	
+
+
 	@Override
 	public void create() throws IOException, NotAvailableException {
-		 URL url = new URL(configuration.getProperty(CONF_BASE_URL));
-		 connection = (HttpURLConnection) url.openConnection();
-		 connection.setChunkedStreamingMode(1024);
-		 connection.setRequestMethod("POST");
-		 connection.setDoOutput(true);
-		 connection.setDoInput(true);
-		 connection.setRequestProperty("Content-Type", "audio/x-raw-int; rate=16000;channels=1;signed=true;endianness=1234;depth=16;width=16");
-		 connection.connect();
-		 
-		 System.out.println("Created connection: " + connection);
+		URL url = new URL(configuration.getProperty(CONF_BASE_URL));
+		connection = (HttpURLConnection) url.openConnection();
+		connection.setChunkedStreamingMode(1024);
+		connection.setRequestMethod("POST");
+		connection.setDoOutput(true);
+		connection.setDoInput(true);
+		connection.setRequestProperty("Content-Type", "audio/x-raw-int; rate=16000;channels=1;signed=true;endianness=1234;depth=16;width=16");
+		connection.connect();
+		out = connection.getOutputStream();
+
+		System.out.println("Created connection: " + connection);
 	}
 
 	@Override
@@ -60,16 +63,16 @@ public class ChunkedWebRecSession implements RecSession {
 	@Override
 	public void sendChunk(byte[] bytes, boolean isLast) throws IOException {
 		if (bytes.length > 0) {
-			connection.getOutputStream().write(bytes);
+			out.write(bytes);
 			System.out.println("Wrote " + bytes.length + " bytes");
 		}
 		if (isLast) {
 			try {
-				connection.getOutputStream().close();
+				out.close();
 				InputStream is = connection.getInputStream();
 				Object obj = JSONValue.parse(new InputStreamReader(is));
 				JSONObject jsonObj=(JSONObject)obj;
-				result = ((JSONArray)((JSONObject)((JSONArray)jsonObj.get("hypotheses")).get(0)).get("utterance")).get(0).toString();
+				result = ((JSONObject)((JSONArray)jsonObj.get("hypotheses")).get(0)).get("utterance").toString();
 			} finally {
 				connection.disconnect();
 				finished = true;
